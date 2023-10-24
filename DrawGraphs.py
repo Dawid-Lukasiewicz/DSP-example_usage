4268a5f1a6ef6ef2e0c96a1620949ae26bf71676
Dawid Łukasiewicz <lukasiewicz@onet.com.pl>
2023-10-24 23:45:40 +0200

    add reading from file to function

--- DrawGraphs.py
+++ DrawGraphs.py
@@ -3,31 +3,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-working_path = Path(".")
-build_path = working_path.joinpath("build-manual")
+def read_data_from_csv(file, n: int) -> [int, float]:
+    if file is None:
+        exit("dupa")
+
+    x = np.arange(0, n, 1, dtype=int)
+    y = np.zeros(n, dtype=float)
+
+    file = open(file, mode="r", newline='')
+
+    csv_reader = csv.reader(file, delimiter=",")
+    i: int = 0
+    for data in csv_reader:
+        for value in data:
+            if value == "":
+                continue
+            y[i] = float(value)
+            i += 1
+
+    file.close()
+    return [x, y]
+
 
 N = 512
-x = np.arange(0, N, 1, dtype=int)
-y = np.zeros(N, dtype=float)
 
-file_name = "signal_file.csv"
-file = open(build_path.joinpath(file_name), mode="r", newline='')
+working_path = Path(".")
+build_path = working_path.joinpath("build-manual")
+file_name = build_path.joinpath("signal_file.csv")
 
-csv_reader = csv.reader(file, delimiter=",")
-i: int = 0
-for data in csv_reader:
-    for value in data:
-        if value == "":
-            continue
-        y[i] = float(value)
-        i += 1
+data = read_data_from_csv(file_name, N)
 
 plt.figure(figsize=(10, 10))
-plt.plot(x, y)
+plt.plot(data[0], data[1])
 plt.xlabel("samples")
 plt.ylabel("values")
 plt.title("signal")
-
-file.close()
 
 plt.show()
